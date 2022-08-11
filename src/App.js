a984c06dc2a3d3e960e0abc2c22e4038c8ccadff
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-11 16:07:53 +0900

    Add; onSubmit value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import logo from "./logo.svg";
+import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "이상호 시청하기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "김민교 시청하기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "철구 시청하기",
+      checked: false,
+    },
+  ]);
+
+  //고윳값으로 사용될 id
+  //ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 }
 
 export default App;
