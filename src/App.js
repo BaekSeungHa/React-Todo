891d6edc606afa98ee69f7897248fc7a2d060cf9
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-11 22:20:16 +0900

    삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
 
   //고윳값으로 사용될 id
   //ref를 사용하여 변수 담기
+
   const nextId = useRef(4);
 
   const onInsert = useCallback(
@@ -40,10 +41,17 @@ function App() {
     [todos]
   );
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 }
